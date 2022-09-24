0ceafb0ac645040b6090af9e7e94eb7c49891e32
riskitriyaniwulandari <114274067+riskitriyaniwulandari@users.noreply.github.com>
2022-09-24 10:39:13 +0700

    Add files via upload

---
+++ arraylatihan4b.php
@@ -0,0 +1,29 @@
+<?php
+$negara = ["Indonesia","Singapura","Malaysia","Brunei","Thailand","Laos","FIlipina","Myanmar"]; 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <title>Latihan4b</title>
+    </head>
+    <body>
+    <p>
+    <b>Daftar Negara ASEAN awal : <br></b>
+        <?php
+        for ($i = 0; $i < 4; $i++)   {
+            echo "<li>$negara[$i]</li>";
+        }
+        ?>
+    </p>
+    <p>
+    <b>Daftar Negara ASEAN Baru : <br></b>
+        <?php
+        for ($i = 0; $i < count($negara); $i++)   {
+            echo "<li>$negara[$i]</li>";
+        }
+        ?>
+    </p>
+    </body>
+</html>
